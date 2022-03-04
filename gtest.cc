3ca7b44f8ab42d7799214d93734e754ce91fbcac
Akanksha <akankshamiharia09@gmail.com>
2022-03-03 23:08:58 -0500

    P2.1 - Added Test cases

--- gtest.cc
+++ gtest.cc
@@ -3,9 +3,9 @@
 #include "BigQ.cc"
 #include "DBFile.h"
 
-TEST(BigQTest1, RecordComparatorTest) {
+TEST(BigQTest1, testRecordComparator) {
     DBFile dbfile;
-	dbfile.Open ("nation.bin");
+	dbfile.Open ("testData/nation.bin");
     Schema* scheme = new Schema("catalog", "nation");
     OrderMaker* order = new OrderMaker(scheme);
     Page bufferPage;
@@ -17,9 +17,9 @@ TEST(BigQTest1, RecordComparatorTest) {
     dbfile.Close();
 }
 
-TEST(BigQTest2, RunComparatorTest) {
+TEST(BigQTest2, testRunComparator) {
     DBFile dbfile;
-	dbfile.Open ("nation.bin");
+	dbfile.Open ("testData/nation.bin");
     Schema* scheme = new Schema("catalog", "nation");
     OrderMaker* order = new OrderMaker(scheme);
     Page bufferPage;
@@ -41,19 +41,63 @@ TEST(BigQTest2, RunComparatorTest) {
     dbfile.Close();
 }
 
-TEST(BigQTest3, CleanUpTest) {
+TEST(BigQTest4, testGetNextRecordOnDifferentPage) {
     DBFile dbfile;
-	dbfile.Open ("nation.bin");
+	dbfile.Open ("testData/nation.bin");
     Schema* scheme = new Schema("catalog", "nation");
-    OrderMaker order (scheme);
-    Pipe in (25);
-    Pipe out (25);
-    Record temp;
-    while(dbfile.GetNext(temp)==1){
-        in.Insert(&temp);
-    }
-    BigQ bigq (in, out, order, 5);
-    EXPECT_FALSE(out.isShutdown());
+    OrderMaker* order = new OrderMaker(scheme);
+    Page bufferPage;
+    Record record1, record2, temp;
+    File file;
+    file.Open(0, "temp");
+    dbfile.GetNext(record1);
+    dbfile.GetNext(record2);
+
+    temp.Copy(&record1);
+    bufferPage.Append(&temp);
+    file.AddPage(&bufferPage, 0);
+    bufferPage.EmptyItOut();
+    temp.Copy(&record2);
+    bufferPage.Append(&temp);
+    file.AddPage(&bufferPage, 1);  
+    class Run* first = new class Run(&file, 0, 2);
+    RunComparator runComparator (order);
+
+    ComparisonEngine c;
+    EXPECT_EQ(c.Compare( first->currRecord, &record1, order), 0);
+    first->getNextRecord();
+    EXPECT_EQ(c.Compare( first->currRecord, &record2, order), 0);
+
+    file.Close();
+    dbfile.Close();
+}
+
+TEST(BigQTest4, testGetNextRecordOnSamePage) {
+    DBFile dbfile;
+	dbfile.Open ("testData/nation.bin");
+    Schema* scheme = new Schema("catalog", "nation");
+    OrderMaker* order = new OrderMaker(scheme);
+    Page bufferPage;
+    Record record1, record2, temp;
+    File file;
+    file.Open(0, "temp");
+    dbfile.GetNext(record1);
+    dbfile.GetNext(record2);
+
+    temp.Copy(&record1);
+    bufferPage.Append(&temp);
+    temp.Copy(&record2);
+    bufferPage.Append(&temp);
+    file.AddPage(&bufferPage, 0);  
+    class Run* first = new class Run(&file, 0, 1);
+
+    ComparisonEngine c;
+    EXPECT_EQ(c.Compare( first->currRecord, &record1, order), 0);
+    first->getNextRecord();
+    EXPECT_EQ(c.Compare( first->currRecord, &record2, order), 0);
+
+    file.Close();
+    dbfile.Close();
 }
 
 
